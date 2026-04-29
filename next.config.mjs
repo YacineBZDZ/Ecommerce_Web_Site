1469a7894033c47982f0bb3e0b4f1d565b6be27c
YacineBZDZ <jakmca18@gmail.com>
2026-04-29 11:26:40 +0200

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
   reactStrictMode: true,
-};
+  basePath: '/ecommerce', // Replace with your repo name
+  assetPrefix: '/ecommerce/',
+  images: {
+    unoptimized: true, // Important for static export
+  },
+  trailingSlash: true,
+}
 
-export default nextConfig;
+module.exports = nextConfig
