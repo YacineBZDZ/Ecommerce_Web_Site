f28141659ebcd687c523ffe8c10a4da4a056448a
YacineBZDZ <jakmca18@gmail.com>
2026-04-30 13:02:29 +0200

    css resolved

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: '/Ecommerce_Web_Site',
-  assetPrefix: '/Ecommerce_Web_Site/',
+  assetPrefix: '/Ecommerce_Web_Site',
   images: {
     unoptimized: true,
   },
